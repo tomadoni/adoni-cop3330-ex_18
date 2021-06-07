f5f84a352d226f92d312311b810f36ef8b0121bc
Tom Adoni <tomad101@gmail.com>
2021-06-07 18:20:10 -0400

    Initial commit

---
+++ src/exercise18.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner kb= new Scanner(System.in);
+	    String temp="";
+	    String choice="";
+	    System.out.println("Press C to convert from Fahrenheit to Celsius.\n"+"Press F to convert from Celsius to Fahrenheit.");
+	    temp= kb.nextLine();
+	    if(temp.equals("C")||temp.equals("c")){
+	        choice="C";
+        }
+	    else{
+	        choice="F";
+        }
+	    System.out.println("Your choice: "+choice);
+	    int t=0;
+	    if(choice.equals("C")) {
+            System.out.println("Please enter the temperature in Fahrenheit: ");
+            t=kb.nextInt();
+            System.out.println("The temperature in Celsius is "+(t-32)*5/9);
+        }
+	    else{
+            System.out.println("Please enter the temperature in Celsius: ");
+            t=kb.nextInt();
+            System.out.println("The temperature in Fahrenheit is "+((t*9/5)+32));
+        }
+    }
+}
